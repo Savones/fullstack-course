4cb0613b9b3eccd2c55db1ab7e821715db31ab77
Essi Savonius <essi.savonius@hotmail.fi>
2024-04-01 20:20:04 +0300

    Finish 2.19

--- osa2/teht18-20/src/App.jsx
+++ osa2/teht18-20/src/App.jsx
@@ -20,37 +20,33 @@ const CountryNames = (props) => {
         Too many matches, specify the filter
       </div>
     )
-  } else if (props.filtered != 1) {
+  } else if (props.filtered != 1 && props.clicked === false) {
     return (
       <div>
         {props.names.map(name =>
-          <CountryName filter={props.filter} name={name} key={name} />
+          <CountryName filter={props.filter} name={name} key={name} onClick={props.onClick} />
         )}
       </div>
     )
   } else {
     return (
       <div>
-        {props.names.map(name =>
-          <CountryInfo countryInfo={props.countryInfo} filter={props.filter} name={name} key={name} />
-        )}
+        <CountryInfo countryInfo={props.countryInfo} filter={props.filter} />
       </div>
     )
   }
 }
 
 const CountryInfo = (props) => {
-  if (props.name.toLowerCase().includes(props.filter.toLowerCase())) {
-    return (
-      <>
-        <h1>{props.countryInfo[0]}</h1>
-        <p>Capital: {props.countryInfo[1]}</p>
-        <p>Area: {props.countryInfo[2]}</p>
-        <p>Languages: {props.countryInfo[4]}</p>
-        {props.countryInfo[3]}
-      </>
-    )
-  }
+  return (
+    <>
+      <h1>{props.countryInfo[0]}</h1>
+      <p>Capital: {props.countryInfo[1]}</p>
+      <p>Area: {props.countryInfo[2]}</p>
+      <p>Languages: {props.countryInfo[4]}</p>
+      {props.countryInfo[3]}
+    </>
+  )
 }
 
 const CountryName = (props) => {
@@ -58,6 +54,7 @@ const CountryName = (props) => {
     return (
       <p>
         {props.name}
+        <button value={props.name} onClick={() => props.onClick(props.name)}>Show</button>
       </p>
     )
   }
@@ -69,6 +66,7 @@ function App() {
   const [filter, setFilter] = useState('')
   const [filtered, setFiltered] = useState(0)
   const [countryInfo, setCountryInfo] = useState([])
+  const [clicked, setClicked] = useState(false)
 
   const hook = () => {
     countryService
@@ -83,6 +81,7 @@ function App() {
   useEffect(hook, [])
 
   const handleFilterChange = (event) => {
+    setClicked(false)
     const newFilter = event.target.value.toLowerCase()
     setFilter(newFilter)
     const filteredCount = countries.filter(name => name.toLowerCase().includes(newFilter)).length
@@ -102,11 +101,26 @@ function App() {
     }
   }
 
+  const handleButtonPress = (name) => {
+    countryService
+      .getInfo(name)
+      .then(responseData => {
+        setCountryInfo([
+          responseData.name.common,
+          responseData.capital,
+          responseData.area,
+          responseData.flag,
+          Object.values(responseData.languages).join(", ")
+        ])
+      })
+    setClicked(true)
+  }
+
 
   return (
     <>
       <Filter value={filter} handleFilterChange={handleFilterChange} />
-      <CountryNames countryInfo={countryInfo} filtered={filtered} filter={filter} names={countries} />
+      <CountryNames clicked={clicked} onClick={handleButtonPress} countryInfo={countryInfo} filtered={filtered} filter={filter} names={countries} />
     </>
   )
 }
