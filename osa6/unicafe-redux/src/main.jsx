5333e1f61852277f2ccae430e075b846f63851f2
Essi Savonius <essi.savonius@hotmail.fi>
2026-01-30 15:15:29 +0200

    6.2

--- osa6/unicafe-redux/src/main.jsx
+++ osa6/unicafe-redux/src/main.jsx
@@ -11,9 +11,9 @@ const App = () => {
       <button onClick={() => store.dispatch({ type: 'OK' })}>ok</button>
       <button onClick={() => store.dispatch({ type: 'BAD' })}>bad</button>
       <button onClick={() => store.dispatch({ type: 'RESET' })}>reset stats</button>
-      <div>good {store.getState().good}</div>
-      <div>ok {store.getState().ok}</div>
-      <div>bad {store.getState().bad}</div>
+      <div>good: {store.getState().good}</div>
+      <div>ok: {store.getState().ok}</div>
+      <div>bad: {store.getState().bad}</div>
     </div>
   )
 }
